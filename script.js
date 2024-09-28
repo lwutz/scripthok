a57544adca539b302f93561e59aa2852b7c126f5
lwutz <aiwudbidub108uboidnaod@proton.me>
2024-09-28 19:30:14 +0200

    hhh

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const fileList = document.getElementById('file-list');
 const leakedSourcesButton = document.getElementById('leaked-sources');
 const cs2CheatsButton = document.getElementById('cs2-cheats');
 
-// File items for Leaked Source Codes
 const leakedSources = [
     {
         name: 'Old Fataliy Source Code',
@@ -11,14 +10,13 @@ const leakedSources = [
         hybridAnalysis: 'https://www.hybrid-analysis.com'
     },
     {
-        name: 'Source Code 2',
+        name: 'COMING SOON',
         downloadLink: 'downloads/source2.zip',
         virusTotal: 'https://www.virustotal.com/gui/file/source2.zip',
         hybridAnalysis: 'https://www.hybrid-analysis.com/sample/source2.zip'
     }
 ];
 
-// File items for CS2 Cheat Crack DLLs
 const cs2Cheats = [
     {
         name: 'Plague Crack',
@@ -27,14 +25,20 @@ const cs2Cheats = [
         hybridAnalysis: 'https://www.hybrid-analysis.com/sample/f2ecabc649b7db40d38a85bbdb8e4491adb613d242bbda3be3677975820d7268/66f74a95174e04b9ef0539b6'
     },
     {
-        name: 'Cheat Crack 2',
-        downloadLink: 'downloads/cheat2.zip',
+        name: 'Weave cs2',
+        downloadLink: 'https://cdn.discordapp.com/attachments/1289226333611753495/1289228876525862975/weave_fix_cracked_.dll?ex=66f960f4&is=66f80f74&hm=fee769f95cf1270bd2668c133b60a28f6cf85a8b360fdaadc4e16d02a92d213e&',
+        virusTotal: 'https://www.virustotal.com/gui/file/cheat2.zip',
+        hybridAnalysis: 'https://www.hybrid-analysis.com/sample/cheat2.zip'
+    }
+    {
+        name: 'COMING SOON',
+        downloadLink: '',
         virusTotal: 'https://www.virustotal.com/gui/file/cheat2.zip',
         hybridAnalysis: 'https://www.hybrid-analysis.com/sample/cheat2.zip'
     }
 ];
 
-// Populate file list based on selection
+
 function populateFileList(files) {
     fileList.innerHTML = ''; // Clear previous items
     files.forEach(file => {
@@ -49,14 +53,14 @@ function populateFileList(files) {
         `;
         fileList.appendChild(fileItem);
     });
-    fileList.classList.remove('hidden'); // Show the file list
+    fileList.classList.remove('hidden'); 
 }
 
-// Event listeners for category buttons
+
 leakedSourcesButton.addEventListener('click', () => {
-    populateFileList(leakedSources); // Populate with leaked sources
+    populateFileList(leakedSources); 
 });
 
 cs2CheatsButton.addEventListener('click', () => {
-    populateFileList(cs2Cheats); // Populate with CS2 cheats
+    populateFileList(cs2Cheats); 
 });
